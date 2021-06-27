10a0d173c4c041571183477199b2f025ba1719f7
dtoyota1226jp <59591878+dtoyota1226jp@users.noreply.github.com>
2021-06-27 18:43:25 +0900

    Add files via upload

---
+++ MyAzureStorageLibraryTest/MockTableStorageService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using MyAzureStoragelibrary;
+
+namespace MyAzureStoragelibraryTest
+{
+    internal class MockTableStorageService : ITableStorageService
+    {
+        private List<TableEntity> _dataSource;
+        public MockTableStorageService(List<TableEntity> dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        public TableStorageServiceResult Get(string continuationToken)
+        {
+            int offset;
+            string newContinuationToken;
+
+            if (string.IsNullOrEmpty(continuationToken))
+            {
+                offset = 0;
+                newContinuationToken = "token1";
+            }
+            else if(continuationToken == "token1")
+            {
+                offset = 1000;
+                newContinuationToken = "token2";
+            }
+            else
+            {
+                offset = 2000;
+                newContinuationToken = "";
+            }
+
+            int count = Math.Min(_dataSource.Count - offset, 1000);
+
+            return new TableStorageServiceResult
+            {
+               Entities = _dataSource.GetRange(offset, count),
+               ContinuationToken = newContinuationToken,
+        };
+        }
+    }
+}
